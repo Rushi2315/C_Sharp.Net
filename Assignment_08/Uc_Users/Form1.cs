ff2bfe56193a17a21ceaf0c95df293e9cf61f277
Rushi <deshmukhrushikesh51@gmail.com>
2025-11-21 16:38:44 +0530

    Assignment-8

---
+++ Assignment_08/Uc_Users/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uc_Users
+{
+    public partial class frm_Login : Form
+    {
+        public frm_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            frm_Main_Form Obj = new frm_Main_Form();
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void frm_Login_Load(object sender, EventArgs e)
+        {
+
+            cmb_User_Role.SelectedIndex = 0;
+            cmb_User_Name.Text = "OMKAR";
+
+            tb_Password.Focus();
+        }
+
+        private void cmb_User_Role_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Shared_Class.Bind_ComboBox(cmb_User_Name, "Username", "Select Distinct(Username) From Login where User_Role = '" + cmb_User_Role.Text + "' ");
+
+        }
+    }
+}
---
+++ Assignment_08/Uc_Users/frm_Main_Form.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uc_Users
+{
+    public partial class frm_Main_Form : Form
+    {
+        public frm_Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Customer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_User_Management_Click(object sender, EventArgs e)
+        {
+            UC.AddUser Obj = new UC.AddUser();
+
+            pnl_Container.Controls.Clear();
+            pnl_Container.Controls.Add(Obj);
+            Obj.Show();
+
+            UserControl UCH = new UserControl();
+
+            pnl_Header.Controls.Clear();
+            pnl_Header.Controls.Add(UCH);
+            UCH.Show();
+        }
+    }
+}
